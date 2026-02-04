94d25ac9908c3acd55e48d0985f684731144540d
Zepipe <akrilkrinov@gmail.com>
2026-02-04 22:51:49 +0300

    Update difference_of_squares.go
    Optimized version

--- solutions/go/difference-of-squares/1/difference_of_squares.go
+++ solutions/go/difference-of-squares/1/difference_of_squares.go
@@ -1,31 +1,20 @@
 package diffsquares
-
 func SquareOfSum(n int) int {
-    var resultSum int
+    resultSum := 0
     
-	if n == 0 {
-        return 0
-    }
-
     for i := 1; i <= n; i++ {
-        resultSum = resultSum + i
+        resultSum += i
     }
     return resultSum * resultSum
 }
-
 func SumOfSquares(n int) int {
-	var resultSum int
+	resultSum := 0
     
-	if n == 0 {
-        return 0
-    }
-
     for i := 1; i <= n; i++ {
-        resultSum = resultSum + i * i
+        resultSum += i * i
     }
     return resultSum
 }
-
 func Difference(n int) int {
 	return SquareOfSum(n) - SumOfSquares(n)
 }
